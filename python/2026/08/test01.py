b77ec5edc23864062ee51d0e8491eba1eababd57
yamonge <et3566@naver.com>
2026-08-10 16:44:46 +0900

    20260810-박유정/numpy python공부

--- python/2026/08/test01.py
+++ python/2026/08/test01.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 students = [
   {"name" : "철수", "score" : 80},
@@ -7,6 +8,11 @@ students = [
 ]
 
 new_students = []
+
+arr = np.array([
+  [10, 20, 30],
+  [40, 50, 60]
+])
 
 # 매개변수로 리스트를 받고 평균 계산후 return 반환 하기
 def get_avg(students):
@@ -28,6 +34,22 @@ def save_students(students):
     json.dump(new_students, file, ensure_ascii=False, indent= 2)
   return True
 
+# 매개변수로 파일이름을 받아 파일이있는지 없는지 확인후 예외 처리 및 변수 저장
+def load_students(file_name):
+  try:
+    with open(file_name, "r", encoding="utf-8") as file: 
+      students = json.load(file)
+      return students
+    
+  except FileNotFoundError:
+    print(f"파일을 찾을수 없습니다.")
+    return False
+
+
+
 print(f"평균 : {get_avg(students)}")
 if(save_students(students)):
   print(f"성공적으로 저장 되었습니다.")
+students = load_students("result2.json")
+print(arr[:, 0:2])
+print(arr[1, :] + 10)
